357bf3deb6cc806fd7acbabd2436972a0423a177
Colin Fendrick <colin.fendrick@gmail.com>
2019-12-24 17:29:12 -0500

    :pencil: Document basics and compile

--- helloworld/main.go
+++ helloworld/main.go
@@ -1,7 +1,7 @@
-package main
+package main // "Main" is used to create an executable package
+// Any other package name is used as a helper package
+import "fmt" // Access the fmt or format package
 
-import "fmt"
-
-func main() {
+func main() { // Declare function "main" that will run when compiled
 	fmt.Println("Hello world")
 }
