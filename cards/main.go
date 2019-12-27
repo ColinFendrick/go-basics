345cb58c11dc7ce5b4157df5bec1b39cd6aa47c1
Colin Fendrick <colin.fendrick@gmail.com>
2019-12-27 11:12:49 -0500

    Create a slice and iterate

--- cards/main.go
+++ cards/main.go
@@ -3,7 +3,14 @@ package main
 import "fmt"
 
 func main() {
-	// var card string = "Ace of Spaces"
-	card := "Ace of Spaces"
-	fmt.Println(card)
+	cards := []string{newCard(), "Ace of Diamonds"} // Create a slice of strings
+	cards = append(cards, "Six of spades")          // Append another element
+	// Iterate over cards
+	for i, card := range cards {
+		fmt.Println(i, card)
+	}
+}
+
+func newCard() string {
+	return "Five of Diamonds"
 }
