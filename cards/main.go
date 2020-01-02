1ec80178d95c07b8968031e268f725029a59d2dd
Colin Fendrick <colin.fendrick@gmail.com>
2020-01-02 08:37:37 -0500

    Make the filenames variables

--- cards/main.go
+++ cards/main.go
@@ -7,10 +7,12 @@ func main() {
 	hand, remainingDeck := deal(cards, 5) // Deal five cards
 	hand.print()                          // Print the hand
 	fmt.Println("\n----------- \nRemaining cards:\n ")
-	remainingDeck.print()                     // Print the rest of the cards
-	hand.saveToFile("hand")                   // Save the hand to file
-	remainingDeck.saveToFile("remainingDeck") // Save the deck
-	readHand := newDeckFromFile("hand")       // Get the hand from file
+	remainingDeck.print() // Print the rest of the cards
+
+	handFileName, remainingFileName := "hand", "remainingDeck" // Create filenames
+	hand.saveToFile(handFileName)                              // Save the hand to file
+	remainingDeck.saveToFile(remainingFileName)                // Save the deck
+	readHand := newDeckFromFile(handFileName)                  // Get the hand from file
 	fmt.Println("\nHand from file\n ")
 	readHand.print() // Print the hand from file (same as hand above)
 
