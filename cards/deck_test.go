908b7c0d7dc09151a2fa6e5c58b91c55b90ff5d7
Colin Fendrick <colin.fendrick@gmail.com>
2020-01-03 08:24:10 -0500

    :white_check_mark: Add test for deal and shuffle

--- cards/deck_test.go
+++ cards/deck_test.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"math/rand"
 	"os"
 	"testing"
+	"time"
 )
 
 func TestNewDeck(t *testing.T) {
@@ -26,14 +28,50 @@ func TestSaveToDeckAndNewDeckFromFile(t *testing.T) {
 
 	os.Remove(filename)
 
-	deck := newDeck()
-	deck.saveToFile(filename)
+	d := newDeck()
+	d.saveToFile(filename)
 
 	loadedDeck := newDeckFromFile(filename)
 
 	if len(loadedDeck) != 52 {
-		t.Errorf("Expected deck length of 52, but got %v", len(loadedDeck))
+		t.Errorf("Expected deck length of 52, but got %d", len(loadedDeck))
 	}
 
 	os.Remove(filename)
+}
+
+func TestShuffle(t *testing.T) {
+	d := newDeck()
+	d.shuffle()
+
+	if d[0] == "Ace of Spades" && d[1] == "Two of Spades" {
+		t.Errorf("Your deck may not be shuffled correctly. Please run this test again. If problem persists, check the shuffle() fn.")
+	}
+
+	hand, _ := deal(d, 5)
+
+	if len(hand) != 5 {
+		t.Errorf("Expected hand length of 5, got %d", len(hand))
+	}
+}
+
+func TestDeal(t *testing.T) {
+	d := newDeck()
+
+	// Test with random length of hand each time
+	source := rand.NewSource(time.Now().UnixNano())
+	r := rand.New(source)
+
+	handLength := r.Intn(len(d) - 1)
+	remLength := len(d) - handLength
+
+	hand, remaining := deal(d, handLength)
+
+	if len(hand) != handLength {
+		t.Errorf("Expected hand length of %v, got %v", handLength, len(hand))
+	}
+
+	if len(remaining) != remLength {
+		t.Errorf("Expected remaining length of %d, got %d", remLength, len(remaining))
+	}
 }
