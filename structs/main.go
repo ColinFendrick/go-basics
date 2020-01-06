cda9c5c3cb56b1d41d4002d0e4c56ccf5fd32f21
Colin Fendrick <colin.fendrick@gmail.com>
2020-01-06 08:03:53 -0500

    :pencil: Add notes on pointers and memory addresses

--- structs/main.go
+++ structs/main.go
@@ -32,9 +32,18 @@ func main() {
 	alex.lastName = "Jones"
 	alex.contactInfo = contactInfo{email: "Alex@infowars.com", zipCode: 12345}
 
-	jim.updateFirstName("jimmy")
+	// & operator creates a pointer directly to the memory location
+	jimPointer := &jim
+
+	// The & is unnecessary here, we can just use jim directly
+	jimPointer.updateFirstName("Jimmy")
+	jim.updateFirstName("Jimmy")
+
 	// Can also update the value directly if we want
+	// Using either pointer or directly
+	jimPointer.lastName = "UpdatedLastName"
 	jim.lastName = "UpdatedLastName"
+
 	jim.print()
 	fmt.Println("\n ")
 	alex.print()
@@ -43,8 +52,9 @@ func main() {
 // Go is a pass by value language, so when the function is called with the struct
 // go copies that value and uses the copy for the function call, rather than the original.
 // So to modify a value on the original we must use a pointer to reference the original value.
-
+// *person means the function is taking a pointer to a person
 func (p *person) updateFirstName(newName string) {
+	// *p looks directly at the value of the pointer address
 	(*p).firstName = newName
 }
 
