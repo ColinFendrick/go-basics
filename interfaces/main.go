1fb48eb9be9f6c05f39b4c38441f8d2441d7bdf0
Colin Fendrick <colin.fendrick@gmail.com>
2020-01-08 06:04:19 -0500

    :pencil: Document interfaces

--- interfaces/main.go
+++ interfaces/main.go
@@ -2,8 +2,9 @@ package main
 
 import "fmt"
 
+// Interfaces are a way to create generic logic for different types
 type bot interface {
-	getGreeting() string
+	getGreeting() string // Every bot will have the getGreeting() fn
 }
 
 type englishBot struct{}
@@ -17,14 +18,18 @@ func main() {
 	printGreeting(sb)
 }
 
-func printGreeting(b bot) {
-	fmt.Println(b.getGreeting())
-}
-
+/* In a real example, we would put the greeting value
+on the struct and make them the same type.
+This is a contrived example. */
 func (b englishBot) getGreeting() string {
 	return "Hello"
 }
 
-func (b spanishBot) getGreeting() string {
+func (spanishBot) getGreeting() string { // Can omit the variable for the receiver if it is not used
 	return "Hola"
+}
+
+// Even though the bots are different types, they both have a getGreeting() fn
+func printGreeting(b bot) {
+	fmt.Println(b.getGreeting())
 }
