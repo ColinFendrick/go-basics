6dd834cfcbb17bc504b5864241d65bd7a988562f
Colin Fendrick <colin.fendrick@gmail.com>
2020-01-08 13:54:04 -0500

    :bug: Fix

--- http/main.go
+++ http/main.go
@@ -69,6 +69,6 @@ func main() {
 
 func (logWriter) Write(bs []byte) (int, error) {
 	fmt.Println(string(bs))
-	fmt.Println("Just wrote this many byes:", len(bs))
+	fmt.Println("Just wrote this many bytes:", len(bs))
 	return len(bs), nil
 }
